c6075754076872c9369083e8ce24d2a5f8acda74
aryan <asin0100@student.monash.edu>
2023-05-01 18:32:39 +1000

    added azure_deploy_simple.sh

---
+++ azure_deploy_simple.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Set variables
+subscriptionId="da6610eb-a343-415c-82fd-4fb305acf34f"
+clientId="c49ca836-941b-41a9-9079-b0438382892e"
+clientSecret="kwF8Q~ba.Aw6pim7S2QzXSCBQaKoO552HthfYc~f"
+tenantId="83e78746-9795-4c9a-bac6-8da174d60c22"
+resourceGroupName="rg_test"
+location="australiaeast"
+
+# Login to Azure using service principal
+az login --service-principal --username $clientId --password $clientSecret --tenant $tenantId
+
+# Set the current subscription
+az account set --subscription $subscriptionId
+
+# Create a resource group
+az group create --name $resourceGroupName --location $location
