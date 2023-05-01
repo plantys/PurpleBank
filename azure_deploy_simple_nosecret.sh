c0e454c3a984ec737d0b2584d21d90bcf45d6ff8
aryan <asin0100@student.monash.edu>
2023-05-01 19:33:52 +1000

    added azure_deploy_simple_nosecret.sh (rg=rg_test1)

--- azure_deploy_simple_nosecret.sh
+++ azure_deploy_simple_nosecret.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # Set variables
-subscriptionId="da6610eb-a343-415c-82fd-4fb305acf34f"
-clientId="c49ca836-941b-41a9-9079-b0438382892e"
-clientSecret="kwF8Q~ba.Aw6pim7S2QzXSCBQaKoO552HthfYc~f"
-tenantId="83e78746-9795-4c9a-bac6-8da174d60c22"
-resourceGroupName="rg_test"
+resourceGroupName="rg_test1"
 location="eastus"
 
 # Login to Azure using service principal
