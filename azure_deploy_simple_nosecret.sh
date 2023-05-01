3e1ac0c85659618fe808d8b81ee220c525ba3d7b
aryan <asin0100@student.monash.edu>
2023-05-01 21:04:14 +1000

    Only azure_deploy.sh is active. rest of *.sh have been commented out.

--- azure_deploy_simple_nosecret.sh
+++ azure_deploy_simple_nosecret.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Set variables
-resourceGroupName="rg_test1"
-location="eastus"
+#resourceGroupName="rg_test1"
+#location="eastus"
 
 # Login to Azure using service principal
-az login --service-principal --username $clientId --password $clientSecret --tenant $tenantId
+#az login --service-principal --username $clientId --password $clientSecret --tenant $tenantId
 
 # Set the current subscription
-az account set --subscription $subscriptionId
+#az account set --subscription $subscriptionId
 
 # Create a resource group
-az group create --name $resourceGroupName --location $location
+#az group create --name $resourceGroupName --location $location
